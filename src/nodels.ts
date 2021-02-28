5454aff86055aa43545d0646b05f4bf4c6b9f6ca
muroon <toyohito.muro@gmail.com>
2021-02-28 15:31:52 +0900

    fix from require

--- src/nodels.ts
+++ src/nodels.ts
@@ -1,10 +1,10 @@
-import path = require("path");
-import fs = require("fs");
+import path from "path";
+import * as fs from "fs";
 
 export const ls = (dirPath: string) => {
     fs.readdir(dirPath, (err: NodeJS.ErrnoException | null, files: string[]) => {
         files.forEach((item: string) => {
-            var itemPath = path.resolve(item);
+            const itemPath = path.resolve(item);
             console.log(itemPath);
         });
     });
